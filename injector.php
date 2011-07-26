1764b4b3d79c886f846cc30b834c30d8627dd59b
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-07-26 19:16:16 +0300

    set language based on HTTP GET argument; set company name; callback for apartment update / create

--- injector.php
+++ injector.php
@@ -18,7 +18,9 @@ class eu_urho_apartments_injector
     var $midgardmvc = null;
 
     /**
-     * Some template hack
+     * Some template magic, setting language, company and user names
+     *
+     * @param object midgardmvc_core_request object
      */
     public function inject_template(midgardmvc_core_request $request)
     {
@@ -30,6 +32,21 @@ class eu_urho_apartments_injector
         $this->midgardmvc->i18n->set_translation_domain($this->component);
         $this->midgardmvc->i18n->set_language($this->midgardmvc->configuration->default_language, false);
 
+        if (isset($_GET['lang']))
+        {
+            try
+            {
+                $this->midgardmvc->i18n->set_language($_GET['lang'], false);
+            }
+            catch (Exception $e)
+            {
+                // Check your locale configuration!
+                // A requested language should be supported by the host server.
+                // Revert to default
+                $this->midgardmvc->i18n->set_language($this->midgardmvc->configuration->default_language, false);
+            }
+        }
+
         $route = $request->get_route();
 
         $route->template_aliases['loginbox'] = 'eua-loginbox';
@@ -39,6 +56,24 @@ class eu_urho_apartments_injector
             $request->set_data_item('eua_user', $this->midgardmvc->authentication->get_person());
             $route->template_aliases['loginbox'] = 'eua-userbox';
         }
+
+        // set the company for the overall site
+        $request->set_data_item('company', $this->midgardmvc->configuration->company);
+    }
+
+    /**
+     * Callback when creating or updating an apartment.
+     * Apartment name needs some care.
+     *
+     * @param object eu_urho_apartments_apartment object
+     */
+    public static function create_url(eu_urho_apartments_apartment $apartment)
+    {
+        if ($apartment->name)
+        {
+            return;
+        }
+        $apartment->name = midgardmvc_helper_urlize::string($apartment->title);
     }
 }
 ?>
